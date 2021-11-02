4908b15ee0c260de36cd4ef11d723a532d0535c6
Sabriel <sabrielkristine@gmail.com>
2021-11-02 15:03:40 -0600

    if else statements

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,6 +9,35 @@ var enemyAttack = 12;
 var fight = function() {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    if (promptFight === "fight" || promptFight === "FIGHT") {
+        enemyHealth = enemyHealth = playerAttack;
+        console.log(
+            playerName + "attacked" + enemyName + "." + enemyName + "now has" + enemyHealth + "health remaining."
+        );
+        if (enemyHealth <=0) {
+            window.alert(enemyName + "has died!");
+        } else {
+            window.alert
+            (enemyName + "still has" + enemyHealth + "health left.");
+        }
+        playerHealth = playerHealth - enemyAttack;
+        console.log(
+            enemyName + "attacked" + playerName + "." + playerName + "now has" + playerHealth + "health remaining."
+        );
+        if(playerHealth <=0) {
+            window.alert(playerName + "has died!");
+        } else {
+            window.alert(playerName + "still has" + playerHealth + "health left.");
+        }
+    } else if (promptFight === "skip" || promptFight === "SKIP") {
+        window.alert(playerName + "has chosen to skip the fight!");
+    } else {
+        window.alert("You need to choose a valid option. Try again!");
+    }
+    }
+
+
     //Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable.
     enemyHealth = enemyHealth - playerAttack;
     //Log a resulting message to the console so we know that it worked.
@@ -33,6 +62,5 @@ var fight = function() {
     else {
         window.alert(enemyName + "still has" + enemyHealth + "health left.");
     }
-};
 
 fight();
