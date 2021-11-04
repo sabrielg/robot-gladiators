5f6f35a29222e867fd9c5f8b85df946ba811bfa3
Sabriel <sabrielkristine@gmail.com>
2021-11-03 20:28:56 -0600

    array and loop fixes

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,10 +10,10 @@ var enemyAttack = 12;
 console.log(enemyNames);
 console.log(enemyNames.length);
 console.log(enemyNames[0]);
-console.log(enemyNames[3]);
+console.log(enemyNames[2]);
 
 
-var fight = function(_enemyName) {
+var fight = function(enemyName) {
     //Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
 
@@ -22,24 +22,24 @@ var fight = function(_enemyName) {
     if (promptFight === "fight" || promptFight === "FIGHT") {
         enemyHealth = enemyHealth - playerAttack;
         console.log(
-            playerName + " attacked" + enemyNames + "." + enemyNames + " now has " + enemyHealth + " health remaining."
+            playerName + " attacked" + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
         );
 
         if (enemyHealth <=0) {
-            window.alert(enemyNames + " has died!");
+            window.alert(enemyName + " has died!");
         } else {
-            window.alert(enemyNames + " still has " + enemyHealth + " health left.");
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
         }
 
         playerHealth = playerHealth - enemyAttack;
         console.log(
-            enemyNames + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
+            enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
         );
 
         if(playerHealth <=0) {
             window.alert(playerName + " has died! ");
         } else {
-            window.alert(playerName + " still has" + playerHealth + " health left.");
+            window.alert(playerName + " still has " + playerHealth + " health left. ");
         }
     } else if (promptFight === "skip" || promptFight === "SKIP") {
         //confirm player want to skip
@@ -51,11 +51,12 @@ var fight = function(_enemyName) {
         }
 
         else {
-            fight ();
+            fight (enemyName);
         }
 
     } else {
         window.alert("You need to choose a valid option. Try again!");
+        fight (enemyName);
     }
 };
 
